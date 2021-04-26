16b1973e7df2467793a51a54a11b7be8eb8b6ccc
Agung Sepruloh <agungsepruloh1996@gmail.com>
2021-04-27 00:37:45 +0700

    Enabled Data Binding and create LoginFragment

---
+++ app/src/main/java/com/agung/latihan2bloodshare/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.agung.latihan2bloodshare
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.agung.latihan2bloodshare.databinding.FragmentLoginBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+}
--- app/src/main/java/com/agung/latihan2bloodshare/MainActivity.kt
+++ app/src/main/java/com/agung/latihan2bloodshare/MainActivity.kt
@@ -2,10 +2,12 @@ package com.agung.latihan2bloodshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.agung.latihan2bloodshare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
