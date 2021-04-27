45fe32b5dbbbbc64f5f0526f8c78af21406cc486
Agung Sepruloh <agungsepruloh1996@gmail.com>
2021-04-27 14:56:31 +0700

    Create register fragment + Navigate from login to register fragment

--- app/src/main/java/com/agung/latihan2bloodshare/RegisterFragment.kt
+++ app/src/main/java/com/agung/latihan2bloodshare/RegisterFragment.kt
@@ -5,26 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
-import com.agung.latihan2bloodshare.databinding.FragmentLoginBinding
+import com.agung.latihan2bloodshare.databinding.FragmentRegisterBinding
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
+ * Use the [RegisterFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginFragment : Fragment() {
+class RegisterFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
-            inflater, R.layout.fragment_login, container, false
-        )
 
-        binding.registerTxt?.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_registerFragment)
+        val binding = DataBindingUtil.inflate<FragmentRegisterBinding>(
+            inflater, R.layout.fragment_register, container, false
         )
 
         return binding.root
