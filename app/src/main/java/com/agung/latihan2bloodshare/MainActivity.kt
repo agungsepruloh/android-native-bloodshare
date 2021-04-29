79da1993a71b4ad84413f96709dfa1ceea5c41f3
Agung Sepruloh <agungsepruloh1996@gmail.com>
2021-04-29 11:03:27 +0700

    Add personal info in the main class

--- app/src/main/java/com/agung/latihan2bloodshare/MainActivity.kt
+++ app/src/main/java/com/agung/latihan2bloodshare/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.agung.latihan2bloodshare
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.agung.latihan2bloodshare.databinding.ActivityMainBinding
 
+/**
+ *
+ * =========================================================================================
+ * This project was created to fulfill the assignment of Mobile Computing Application course.
+ *
+ *                              Universitas Komputer Indonesia
+ * =========================================================================================
+ *
+ * Author's Information
+ * ==========================
+ * NIM      : 10118902
+ * Name     : Agung Sepruloh
+ * Class    : IF-11
+ * Informatics Engineering
+ * ==========================
+ * Created on  27 Apr 2021
+ *
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
